2870880af739383e9f29b91ff9128d413f101f58
Sabrina Zancotti <zancotti@gmail.com>
2022-01-31 12:00:49 +0100

    Addded export to the url.

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,4 @@
-const Base_URL = 'https://murdermystery-backend.herokuapp.com';
+export const Base_URL = 'https://murdermystery-backend.herokuapp.com';
 // export const Base_URL = 'http://localhost:8080';
 
 export const API_URL = slug => `${Base_URL}/${slug}`;
